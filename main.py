412e713802a76e8ac084631d1099b4273a9a1f45
No767 <73260931+No767@users.noreply.github.com>
2021-07-22 02:02:58 -0700

    Test out info cmd

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from discord import Intents
 import os
 from dotenv import load_dotenv
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN") #just grabs the discord token from a .env file
 intents = Intents.all()
-bot = Bot(intents=intents, command_prefix='!') # or whatever prefix you choose(!,%,?)
+bot = Bot(intents=intents, command_prefix='$') # or whatever prefix you choose(!,%,?)
 
+@bot.command(name='info')
+async def fetchServerInfo(context):
+    info = context.info
+    await context.send(f'Server Name: {info.name}')
+    await context.send(f'Server Size: {len(info.members)}')
+    await context.send(f'Server Name: {info.owner.display_name}')
+        
+        
+        
+        
+        
 class MyClient(discord.Client):
     @bot.event
     async def on_message(message):
-	    if message.content == 'hello':
+	    if message.content == '$hello':
 		    await message.channel.send('Hello there')
-    async def on_message(message):
-        if message.content == 'info':
-            await message.channel.send('Some info here')
 
 bot.run(TOKEN) #Make sure not to forget thtis....
