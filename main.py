2d2c80ce1000189fe536b6b14274fe65362ebe99
No767 <73260931+No767@users.noreply.github.com>
2021-08-08 03:43:42 -0700

    Change out default commands and use improved commands

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
-from discord.ext.commands import Bot
 from discord import Intents
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-TOKEN = os.getenv("TOKEN") #just grabs the discord token from a .env file
+TOKEN = os.getenv("TOKEN")  # just grabs the discord token from a .env file
 intents = Intents.all()
-bot = Bot(intents=intents, command_prefix='$') # or whatever prefix you choose(!,%,?)
+bot = commands.Bot(command_prefix=".")
+
 
 @bot.command(name='info')
 async def fetchServerInfo(context):
@@ -14,19 +16,9 @@ async def fetchServerInfo(context):
     await context.send(f'Server Name: {info.name}')
     await context.send(f'Server Size: {len(info.members)}')
     await context.send(f'Server Name: {info.owner.display_name}')
-        
-        
-        
-        
-        
-class MyClient(discord.Client):
-    @bot.event
-    async def on_message(message):
-	    if message.content == 'hello':
-		    await message.channel.send('Hello there')
-    async def on_message(message):
-        if message.content == 'help':
-            await message.channel.send("What is the help that you need?")
-            await message.channel.send("Please contact me for help")
 
-bot.run(TOKEN) #Make sure not to forget thtis....
+@bot.command()
+async def rininfo(ctx):
+    await ctx.send(f'Hello There \nThis is the Rin Bot, a bot which uses the EasyBot.py framework')
+
+bot.run(TOKEN)
