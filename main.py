507541ddab09905b8ea08800c4aedd192a2c5a48
No767 <73260931+No767@users.noreply.github.com>
2021-08-08 23:42:36 -0700

    Add very basic info cmd

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import discord
 from discord import Intents
 from discord.ext import commands
+from discord.abc import User
 import os
 from dotenv import load_dotenv
+import datetime
 
+# Grabs the bot's token from the .env file
 load_dotenv()
-TOKEN = os.getenv("TOKEN")  # just grabs the discord token from a .env file
+TOKEN = os.getenv("TOKEN")  
 intents = Intents.all()
 bot = commands.Bot(command_prefix=".")
 
 
 @bot.command(name='info')
-async def fetchServerInfo(context):
-    info = context.info
-    await context.send(f'Server Name: {info.name}')
-    await context.send(f'Server Size: {len(info.members)}')
-    await context.send(f'Server Name: {info.owner.display_name}')
-
-@bot.command()
-async def rininfo(ctx):
-    await ctx.send(f'Hello There \nThis is the Rin Bot, a bot which uses the EasyBot.py framework')
+async def on_message(message):
+    embedVar = discord.Embed(title="Info", color=14414079, timestamp= datetime.datetime.now())
+    embedVar.add_field(name="Command Prefix", value='Command Prefix is "**.**"')
+    embedVar.add_field(name="Server Name", value=message.guild.name)
+    await message.channel.send(embed=embedVar)
 
 bot.run(TOKEN)
