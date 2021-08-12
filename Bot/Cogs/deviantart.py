326b6cf623a5e6c97f2a8d49a03c99d6deb130b8
No767 <73260931+No767@users.noreply.github.com>
2021-08-12 02:31:06 -0700

    Add in Deviantart Search Function

--- Bot/Cogs/deviantart.py
+++ Bot/Cogs/deviantart.py
@@ -7,19 +7,24 @@ import os
 load_dotenv()
 Client_ID = os.getenv("Client_ID")
 Client_Secret = os.getenv("Client_Secret")
-da = deviantart.Api("Client_ID", "Client_Secret")
+da = deviantart.Api(Client_ID, Client_Secret)
 
-class deviantart(commands.Cog):
+# the devartfetch gives out numbers, will need to get images instead
+class devart(commands.Cog, discord.Client):
     def __init__(self, bot):
         self.bot = bot
     @commands.command(
-        name = 'diviantart',
-        help = 'Grab art from Devianart'
+        name = 'artbrowse'
     )
-    
-    @client.event
-    async def on_message(message):
-        if message.content.startswith('.devianart'):
-            channel = message.channel
-            await channel.send('Please enter in the chat the User that you want to get the art from')
-            
+    async def on_message(message, ctx):
+        await ctx.send(da.browse(endpoint='hot'))
+    async def on_error(message, ctx):
+        await ctx.send("There seems to be an error. Pleas try again")
+        
+    @commands.command(
+        name = 'devartsearch'
+    )
+    async def on_message(message, ctx):
+        await ctx.send(da.search_tags("Anime"))
+def setup(bot):
+    bot.add_cog(devart(bot))
--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -1,6 +1,7 @@
 import discord
 from discord import Intents
 from discord.ext import commands
+from discord import client
 import os
 from dotenv import load_dotenv
 # Grabs the bot's token from the .env file
@@ -10,7 +11,7 @@ intents = Intents.all()
 bot = commands.Bot(command_prefix=".")
 
 # Loads in all extensions
-initial_extensions = ['Cogs.rininfo', 'Cogs.reddit', 'Cogs.plugin_tools', 'Cogs.global', 'Cogs.chat',  'Cogs.images', 'Cogs.rinping']
+initial_extensions = ['Cogs.rininfo', 'Cogs.plugin_tools', 'Cogs.global', 'Cogs.chat', 'Cogs.rinping', 'Cogs.deviantart']
 for extension in initial_extensions:
     bot.load_extension(extension)
 
