c427e0f521466da7315ea1f82f3e0241fe9b1f3e
No767 <73260931+No767@users.noreply.github.com>
2021-08-31 09:33:17 -0700

    Update Pinger Cog

--- Bot/Cogs/pinger.py
+++ Bot/Cogs/pinger.py
@@ -11,7 +11,7 @@ class pinger(commands.Cog):
         pinger_search = search
         for everyone in range(11):
             await ctx.send(f"@everyone {search}")
-
-
+    async def on_error(self, ctx):
+        await ctx.send(f'There is something wrong with the pinger module. Please Try Again...')
 def setup(bot):
     bot.add_cog(pinger(bot))
