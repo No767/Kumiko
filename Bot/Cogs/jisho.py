6d1051525d533d4284e67bf591b1cb4169355096
No767 <73260931+No767@users.noreply.github.com>
2021-09-11 00:59:21 -0700

    Start work on the Jisho API Support

---
+++ Bot/Cogs/jisho.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+import random
+from discord.ext import commands
+import json
+
+link = 'https://jisho.org/api/v1/search/words?keyword=yes'
+def jisho_scrapper(link):
+    r = requests.get(link)
+    jisho_data = r.text
+    jisho_parser = json.loads(jisho_data)
+    print(jisho_parser)
+    
+jisho_scrapper(link)
+
