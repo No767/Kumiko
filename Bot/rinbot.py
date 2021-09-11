909c0c6706f6d82f37d2dbb04c2023e8448a5743
No767 <73260931+No767@users.noreply.github.com>
2021-09-11 12:29:26 -0700

    [skip ci] Add Comment on rinbot.py

--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -36,7 +36,7 @@ initial_extensions = [
 for extension in initial_extensions:
     bot.load_extension(extension)
 
-
+# Adds in the bot presence 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=".rinhelp"))
