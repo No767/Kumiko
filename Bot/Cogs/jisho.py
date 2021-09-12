61b6881c05dafeb1bb1d35f1ff33238d82f42522
No767 <73260931+No767@users.noreply.github.com>
2021-09-12 01:04:41 -0700

    Add in Jisho Data in Discord Embed

--- Bot/Cogs/jisho.py
+++ Bot/Cogs/jisho.py
@@ -5,7 +5,6 @@ import requests
 from discord import Embed
 from discord.ext import commands
 
-
 class jisho_dict(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,12 +20,35 @@ class jisho_dict(commands.Cog):
         embedVar.description = f"""
         First Word: {jisho_parser['data'][0]['japanese'][0]['reading']}
         
-        Senses: {jisho_parser['data'][0]['senses'][0]['english_definitions']}
+        English Def: {jisho_parser['data'][0]['senses'][0]['english_definitions']}
+        
+        Parts of speech: {jisho_parser['data'][0]['senses'][0]['parts_of_speech']}
+        
+        Tags? >> {jisho_parser['data'][0]['tags']}
+        
+        ---
+        
+        The second word: {jisho_parser['data'][1]['japanese'][0]['reading']}
+        
+        That second word's english writing: {jisho_parser['data'][1]['senses'][1]['english_definitions']}
         
         
+        Parts of speech: {jisho_parser['data'][1]['senses'][1]['parts_of_speech']}
         
+        Tags >> {jisho_parser['data'][1]['senses'][1]['tags']}
         
+        --- 
+        That goddamn third word (kanji): {jisho_parser['data'][2]['japanese'][0]['word']}
         
+        Hiragana: {jisho_parser['data'][2]['japanese'][0]['reading']}
+        
+        Katakana: {jisho_parser['data'][2]['japanese'][1]['reading']}
+        
+        English Def: {jisho_parser['data'][2]['senses'][0]['english_definitions']}
+        
+        Parts of Speech: {jisho_parser['data'][2]['senses'][0]['parts_of_speech']}
+        
+        tags: {jisho_parser['data'][2]['senses'][0]['tags']}
         """
         await ctx.send(embed=embedVar)
 
