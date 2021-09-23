bac9f015ffe0d0e7c0a443dacc07aed11b5a4fec
No767 <73260931+No767@users.noreply.github.com>
2021-09-23 00:51:26 -0700

    Add Kanji Alive API Support for Jisho Cog

--- Bot/Cogs/jisho.py
+++ Bot/Cogs/jisho.py
@@ -5,6 +5,10 @@ import requests
 from discord import Embed
 from discord.ext import commands
 import re
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class jisho_dict(commands.Cog):
     def __init__(self, bot):
@@ -14,40 +18,29 @@ class jisho_dict(commands.Cog):
     async def on_message(self, ctx, search: str):
         search = search.replace(" ", "%20")
         link = f"https://jisho.org/api/v1/search/words?keyword={search}"
+        kanji_url = f"https://kanjialive-api.p.rapidapi.com/api/public/search/{search}"
+        headers = {
+            'x-rapidapi-host': "kanjialive-api.p.rapidapi.com",
+            'x-rapidapi-key': f"{os.getenv('Rapid_API_Key')}"
+        }
         r = requests.get(link)
         jisho_data = r.text
         jisho = json.loads(jisho_data)
+        kanji_alive = requests.request("GET", kanji_url, headers=headers)
+        kanji_parser = json.loads(kanji_alive.text)
+        
+
+        
         embedVar = discord.Embed()
-        
-        
         embedVar.description = f"""
         Results: 
-        
-        **Result 1**
-        
-        Kanji >> {str(jisho['data'][0]['slug']).split(", ")[0]}
-        Hiragana >> {jisho['data'][0]['japanese'][0]['reading']}
-        Katakana >> {jisho['data'][0]['japanese'][1]['reading']}
-        
-        
-        English Def >> 
+        Kanji >> {str(kanji_parser).split(", ")}
+
 
         Attributions
         JMDict >> {jisho['data'][0]['attribution']['jmdict']}
         JMNEDict >> {jisho['data'][0]['attribution']['jmnedict']}
         DBPedia >> {jisho['data'][0]['attribution']['dbpedia']}
-        
-        **Result 2**
-        
-        Kanji >> {str(jisho['data'][1]['slug']).split(", ")[0]}
-        Hiragana >> {str(jisho['data'][1]['japanese'][0]['reading']).split(", ")[0]}
-        Katakana >> {str(jisho['data'][1]['japanese'][0]['reading']).split(", ")[0]}
-        
-        English Def >> 
-
-
-        
-
         
         **HTTP Status**
         HTTP Status Code >> {jisho['meta']['status']}
@@ -55,6 +48,10 @@ class jisho_dict(commands.Cog):
         
         """
         await ctx.send(embed=embedVar)
+        
+
+        
+    
 
 
     
