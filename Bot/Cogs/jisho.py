f8a9e69cd39aa40e55afa52e0a40ec8fcf084294
No767 <73260931+No767@users.noreply.github.com>
2021-09-27 00:20:06 -0700

    Use JMDict in order to search up Kanji and English Def

--- Bot/Cogs/jisho.py
+++ Bot/Cogs/jisho.py
@@ -7,10 +7,24 @@ import requests
 from discord import Embed
 from discord.ext import commands
 from dotenv import load_dotenv
+from jamdict import Jamdict
 
 load_dotenv()
+jam = Jamdict()
 
-
+def english_def_parser(search):
+    search = search.replace(" ", "%")
+    result = jam.lookup(search)
+    for word in result.entries:
+        return list(word)
+    
+def kanji_finder(search): 
+    search = search.replace(" ", "%")
+    result = jam.lookup(search)
+    for c in result.chars:
+        searcher = re.search("^\w", repr(c))
+        return searcher.group(0)
+    
 class jisho_dict(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,7 +35,7 @@ class jisho_dict(commands.Cog):
             search = search.replace(" ", "%20")
             link = f"https://jisho.org/api/v1/search/words?keyword={search}"
             kanji_url = (
-                f"https://kanjialive-api.p.rapidapi.com/api/public/search/{search}"
+                f"https://kanjialive-api.p.rapidapi.com/api/public/search/{search}" # May not need Kanji Alive since JMDict is replacing that for the Kanji lookup
             )
             headers = {
                 "x-rapidapi-host": "kanjialive-api.p.rapidapi.com",
@@ -36,8 +50,11 @@ class jisho_dict(commands.Cog):
             embedVar = discord.Embed()
             embedVar.description = f"""
             Results: 
-            Kanji >> {kanji_parser}
 
+            Kanji >> {kanji_finder(search)}
+            
+            
+            English Def >> {english_def_parser(search)}
             Attributions
             JMDict >> {jisho['data'][0]['attribution']['jmdict']}
             JMNEDict >> {jisho['data'][0]['attribution']['jmnedict']}
