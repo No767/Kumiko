47b57ebea93e2345a0359bc2a2430196560a80b6
No767 <73260931+No767@users.noreply.github.com>
2021-10-08 22:16:15 -0700

    Work on Twitter V2

--- Bot/Cogs/twitter_v2.py
+++ Bot/Cogs/twitter_v2.py
@@ -1,16 +1,15 @@
 import os
+import json
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from searchtweets import load_credentials
-
 load_dotenv()
 
 Twitter_API_Key = os.getenv("Twitter_API_Key")
 API_Secret_Key = os.getenv("API_Secret_Key")
 Access_Token = os.getenv("Access_Token")
 Access_Token_Secret = os.getenv("Access_Token_Secret")
+Bearer_Token = os.getenv("Twitter_Bearer_Token")
 
 
-load_credentials()
