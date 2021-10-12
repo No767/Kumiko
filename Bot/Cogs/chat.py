82f9c9b53fa92439e68d977afbf0bfc7e0d0b6f1
No767 <73260931+No767@users.noreply.github.com>
2021-10-12 00:01:01 -0700

    Actually check if the file is there or not
    The chat cog always had an issue where it would give back a "no such file or directory was found". This fixes the issue by actually checking if the file exists or not

--- Bot/Cogs/chat.py
+++ Bot/Cogs/chat.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import discord
 from discord.ext import commands
@@ -19,37 +20,43 @@ class Chat(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot:
-            # message has multiple different sub definitions like content author channel
-            msg = message.content.lower()
-            if msg.startswith(
-                "rinupdate"
-            ):  # Use rinupdate instead, so if someone is saying update in the chat, this will not trigger
-                msg = msg.replace("rinupdate ", "")  # trunicate off update
-                inputs = msg.split(",")
-                try:
-                    msg_responses = dict(ping=inputs[0], pong=inputs[1])
-                    response_file = open("chat/responses.json", "a")
-                    response_file.write(f"{json.dumps(msg_responses)}\n")
-                except Exception as e:
-                    await message.channel.send(
-                        'The syntax is incorrect. Please type in the format "update [input word], [response]". \nThis will make it so that when a user types [input word], the bot will say [response].'
-                    )
-                    print(e)
-            else:
-                try:
-                    msg_save = open("chat/responses.json", "r")
-                    msg_responses = msg_save.readlines()
-                    msg_responses = [
-                        json.loads(s.replace("\n", "")) for s in msg_responses
-                    ]
-                    msg_save.close()
-                    for i in msg_responses:
-                        if i["ping"] == msg:
-                            await message.channel.send(i["pong"])
-                            return
-                except Exception as e:
-                    await message.channel.send(e)
+        try:
+            if not message.author.bot:
+                # message has multiple different sub definitions like content author channel
+                msg = message.content.lower()
+                if msg.startswith(
+                    "rinupdate"
+                ):  # Use rinupdate instead, so if someone is saying update in the chat, this will not trigger
+                    msg = msg.replace("rinupdate ", "")  # trunicate off update
+                    inputs = msg.split(",")
+                    try:
+                        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+                        path_name = "./chat/responses.json"
+                        if os.path.isfile(path_name) == "True":
+                            msg_responses = dict(ping=inputs[0], pong=inputs[1])
+                            response_file = open("./chat/responses.json", "a")
+                            response_file.write(f"{json.dumps(msg_responses)}\n")
+                    except Exception as e:
+                        await message.channel.send(
+                            'The syntax is incorrect. Please type in the format "update [input word], [response]". \nThis will make it so that when a user types [input word], the bot will say [response].'
+                        )
+                        print(e)
+                else:
+                    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+                    path_name = "./chat/responses.json"
+                    if os.path.isfile(path_name) == "True":
+                        msg_save = open("./chat/responses.json", "r")
+                        msg_responses = msg_save.readlines()
+                        msg_responses = [
+                            json.loads(s.replace("\n", "")) for s in msg_responses
+                        ]
+                        msg_save.close()
+                        for i in msg_responses:
+                            if i["ping"] == msg:
+                                await message.channel.send(i["pong"])
+                                return
+        except Exception as e:
+            await message.channel.send(e)
 
 
 def setup(bot):
