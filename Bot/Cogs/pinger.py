d0ac8defb629901c87bf2b95a62a68611726c5f4
No767 <winterpiece104@gmail.com>
2021-11-09 22:59:00 -0800

    Add more checks for global-error-handling + more args checks

--- Bot/Cogs/pinger.py
+++ Bot/Cogs/pinger.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 
 class rinpinger(commands.Cog):
@@ -6,13 +7,22 @@ class rinpinger(commands.Cog):
         self.bot = bot
 
     @commands.command(name="pinger")
-    async def on_message(self, ctx, replace: int, *, reason: str):
+    async def pinger(self, ctx, replace: int, *, reason: str):
         try:
             for _ in range(replace):
                 await ctx.send(f"@everyone {reason}")
         except Exception as e:
             await ctx.send(f"The pinger cog didnt work. Please try again.\nReason: {e}")
 
+    @pinger.error
+    async def on_message_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embedVar = discord.Embed(color=discord.Color.from_rgb(255, 51, 51))
+            embedVar.description = f"Missing a required argument: {error.param}"
+            msg = await ctx.send(embed=embedVar, delete_after=10)
+            await msg.delete(delay=10)
 
 def setup(bot):
     bot.add_cog(rinpinger(bot))
