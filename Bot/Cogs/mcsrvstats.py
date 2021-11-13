bfd634f76ef183d32958c3670636c8146d0b6831
No767 <winterpiece104@gmail.com>
2021-11-12 18:31:48 -0800

    Finish Adding Fields for McSrvStat Cog

--- Bot/Cogs/mcsrvstats.py
+++ Bot/Cogs/mcsrvstats.py
@@ -9,7 +9,7 @@ class mcsrvstats(commands.Cog):
         self.bot = bot
 
     @commands.command(name="javamcsrv")
-    async def on_message(self, ctx, search: str):
+    async def java(self, ctx, search: str):
         search = search.replace(" ", "%20")
         link = f"https://api.mcsrvstat.us/2/{search}"
         image_link = f"https://api.mcsrvstat.us/icon/{search}"
@@ -21,35 +21,6 @@ class mcsrvstats(commands.Cog):
                 embedVar = discord.Embed(
                     title="Infomation (Java Edition)", color=0xC27C0E
                 )
-                embedVar.description = f"""
-                **Infomation (Java Edition)**
-
-                Online Status >> {mcsrv['online']}
-                Hostname/Domain >> {mcsrv['hostname']}
-                IP Address >> {mcsrv['ip']}
-                Port >> {mcsrv['port']}
-
-                **Player Count**
-
-                Players Online >> {mcsrv['players']['online']}
-                Max Online Player Slots >> {mcsrv['players']['max']}
-
-                **MOTD**
-
-                {str(mcsrv['motd']['clean']).replace("[", "").replace("]", "").replace("'", "")}
-
-                **Debug**
-                Ping >> {mcsrv['debug']['ping']}
-                Query >> {mcsrv['debug']['query']}
-                SRV Record >> {mcsrv['debug']['srv']}
-                Query Mismatch >> {mcsrv['debug']['querymismatch']}
-                IP in SRV >> {mcsrv['debug']['ipinsrv']}
-                CNAME in SRV >> {mcsrv['debug']['cnameinsrv']}
-                Animated MOTD >> {mcsrv['debug']['animatedmotd']}
-                Cache Time >> {mcsrv['debug']['cachetime']}
-                API Version >> {mcsrv['debug']['apiversion']}
-                HTTP Status (MCSrvStat) >> {mcsrv_status_code}
-                """
                 embedVar.add_field(
                     name="Online Status", value=mcsrv["online"], inline=True
                 )
@@ -113,27 +84,7 @@ class mcsrvstats(commands.Cog):
                 embedVar.set_thumbnail(url=image_link)
                 await ctx.send(embed=embedVar)
             else:
-                embedVar = discord.Embed(color=0xC27C0E)
-                embedVar.description = f"""
-                **Infomation (Java Edition)**
-            
-                Online Status >> {mcsrv['online']}
-                Hostname/Domain >> {mcsrv['hostname']}
-                IP Address >> {mcsrv['ip']}
-                Port >> {mcsrv['port']}
-            
-                **Debug**
-            
-                Ping >> {mcsrv['debug']['ping']}
-                Query >> {mcsrv['debug']['query']}
-                SRV Record >> {mcsrv['debug']['srv']}
-                Query Mismatch >> {mcsrv['debug']['querymismatch']}
-                IP in SRV >> {mcsrv['debug']['ipinsrv']}
-                CNAME in SRV >> {mcsrv['debug']['cnameinsrv']}
-                Animated MOTD >> {mcsrv['debug']['animatedmotd']}
-                Cache Time >> {mcsrv['debug']['cachetime']}
-                HTTP Status (MCSrvStat) >> {mcsrv_status_code}
-                """
+                embedVar = discord.Embed(title="Infomation (Java Edition)", color=0xC27C0E)
                 embedVar.add_field(name="Online Status", value=mcsrv["online"])
                 embedVar.add_field(
                     name="Hostname/Domain", value=mcsrv["hostname"], inline=True
@@ -171,29 +122,31 @@ class mcsrvstats(commands.Cog):
                     name="Cache Time", value=mcsrv["debug"]["cachetime"], inline=True
                 )
                 embedVar.add_field(
-                    name="API Version", value=mcsrv["debug"]["apiversion"], inline=True
-                )
-                embedVar.add_field(
                     name="HTTP Status (MCSrvStat)", value=mcsrv_status_code, inline=True
                 )
                 embedVar.set_thumbnail(url=image_link)
                 await ctx.send(embed=embedVar)
-                embedVar.set_thumbnail(url=image_link)
-                await ctx.send(embed=embedVar)
         except Exception as e:
             embedVar = discord.Embed(color=0xC27C0E)
-            embedVar.description = f"""
-            Your search for has failed. Please try again.\nReason: {e}
-            """
+            embedVar.description = f"Your search for has failed. Please try again.\nReason: {e}"
             await ctx.send(embed=embedVar)
 
+    @java.error
+    async def on_message_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embedVar = discord.Embed(color=discord.Color.from_rgb(255, 51, 51))
+            embedVar.description = f"Missing a required argument: {error.param}"
+            msg = await ctx.send(embed=embedVar, delete_after=10)
+            await msg.delete(delay=10)
 
 class bedrock_mcsrvstats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="bedrockmcsrv")
-    async def on_message(self, ctx, search: str):
+    async def bedrock(self, ctx, search: str):
         search = search.replace(" ", "%20")
         link = f"https://api.mcsrvstat.us/bedrock/2/{search}"
         bedimage_link = f"https://api.mcsrvstat.us/icon/{search}"
@@ -202,72 +155,58 @@ class bedrock_mcsrvstats(commands.Cog):
         bedmcsrv_status_code = r.status_code
         try:
             if "True" in str(bedmcsrv["online"]):
-                embedVar = discord.Embed(color=0x607D8B)
-                embedVar.description = f"""
-                **Information (Bedrock Edition)**
-
-                Online Status >> {bedmcsrv['online']}
-                Hostname/Domain >> {bedmcsrv['hostname']}
-                IP Address >> {bedmcsrv['ip']}
-                Port >> {bedmcsrv['port']}
-                Supported/Server Version >> {bedmcsrv['version']}
-                Map >> {bedmcsrv['map']}
-
-                **Player Count**
-
-                Players Online >> {bedmcsrv['players']['online']}
-                Max Online Player Slots >> {bedmcsrv['players']['max']}
-
-                **MOTD**
-
-                {str(bedmcsrv['motd']['clean']).replace("[", "").replace("]", "").replace("'", "")}
-
-                **Debug**
-
-                Protocol >> {bedmcsrv['protocol']}
-                Ping >> {bedmcsrv['debug']['ping']}
-                Query >> {bedmcsrv['debug']['query']}
-                Query Mismatch >> {bedmcsrv['debug']['querymismatch']}
-                IP in SRV >> {bedmcsrv['debug']['ipinsrv']}
-                CNAME in SRV >> {bedmcsrv['debug']['cnameinsrv']}
-                Animated MOTD >> {bedmcsrv['debug']['animatedmotd']}
-                Cache Time >> {bedmcsrv['debug']['cachetime']}
-                API Version >> {bedmcsrv['debug']['apiversion']}
-                HTTP Status (MCSrvStat) >> {bedmcsrv_status_code}
-                """
+                embedVar = discord.Embed(title="Information (Bedrock Edition)", color=0x607D8B)
+                embedVar.add_field(name="Online Status", value=bedmcsrv['online'], inline=True)
+                embedVar.add_field(name="Hostname/Domain", value=bedmcsrv['hostname'], inline=True)
+                embedVar.add_field(name="IP Address", value=bedmcsrv['ip'], inline=True)
+                embedVar.add_field(name="Port", value=bedmcsrv['port'], inline=True)
+                embedVar.add_field(name="Supported/Server Version", value=bedmcsrv['version'], inline=True)
+                embedVar.add_field(name="Map", value=bedmcsrv['map'], inline=True)
+                embedVar.add_field(name="Players Online", value=bedmcsrv['players']['online'], inline=True)
+                embedVar.add_field(name="Max Online Player Slots", value=bedmcsrv['players']['max'], inline=True)
+                embedVar.add_field(name="MOTD", value=str(bedmcsrv['motd']['clean']).replace("[", "").replace("]", "").replace("'", ""), inline=True)
+                embedVar.add_field(name="Protocol", value=bedmcsrv['protocol'], inline=True)
+                embedVar.add_field(name="Ping", value=bedmcsrv['debug']['ping'], inline=True)
+                embedVar.add_field(name="Query", value=bedmcsrv['debug']['query'], inline=True)
+                embedVar.add_field(name="Query Mismatch", value=bedmcsrv['debug']['query_mismatch'], inline=True)
+                embedVar.add_field(name="IP in SRV", value=bedmcsrv['debug']['ipinsrv'], inline=True)
+                embedVar.add_field(name="CNAME in SRV", value=bedmcsrv['debug']['cnameinsrv'], inline=True)
+                embedVar.add_field(name="Animated MOTD", value=bedmcsrv['debug']['animatedmotd'], inline=True)
+                embedVar.add_field(name="Cache Time", value=bedmcsrv['debug']['cachetime'], inline=True)
+                embedVar.add_field(name="API Version", value=bedmcsrv['debug']['apiversion'], inline=True)
+                embedVar.add_field(name="HTTP Status (MCSrvStat)", value=bedmcsrv_status_code, inline=True)
                 embedVar.set_thumbnail(url=bedimage_link)
                 await ctx.send(embed=embedVar)
             else:
-                embedVar = discord.Embed(color=0x607D8B)
-                embedVar.description = f"""
-                **Information (Bedrock Edition)**
-
-                Online Status >> {bedmcsrv['online']}
-                Hostname/Domain >> {bedmcsrv['hostname']}
-                IP Address >> {bedmcsrv['ip']}
-                Port >> {bedmcsrv['port']}
-
-                **Debug**
-
-                Ping >> {bedmcsrv['debug']['ping']}
-                Query >> {bedmcsrv['debug']['query']}
-                SRV Record >> {bedmcsrv['debug']['srv']}
-                Query Mismatch >> {bedmcsrv['debug']['querymismatch']}
-                IP in SRV >> {bedmcsrv['debug']['ipinsrv']}
-                CNAME in SRV >> {bedmcsrv['debug']['cnameinsrv']}
-                Animated MOTD >> {bedmcsrv['debug']['animatedmotd']}
-                Cache Time >> {bedmcsrv['debug']['cachetime']}
-                HTTP Status (MCSrvStat) >> {bedmcsrv_status_code}
-                """
+                embedVar = discord.Embed(title="Information (Bedrock Edition)", color=0x607D8B)
+                embedVar.add_field(name="Online Status", value=bedmcsrv['online'], inline=True)
+                embedVar.add_field(name="Hostname/Domain", value=bedmcsrv['hostname'], inline=True)
+                embedVar.add_field(name="IP Address", value=bedmcsrv['ip'], inline=True)
+                embedVar.add_field(name="Port", value=bedmcsrv['port'], inline=True)
+                embedVar.add_field(name="Ping", value=bedmcsrv['debug']['ping'], inline=True)
+                embedVar.add_field(name="Query", value=bedmcsrv['debug']['query'], inline=True)
+                embedVar.add_field(name="Query Mismatch", value=bedmcsrv['debug']['query_mismatch'], inline=True)
+                embedVar.add_field(name="IP in SRV", value=bedmcsrv['debug']['ipinsrv'], inline=True)
+                embedVar.add_field(name="CNAME in SRV", value=bedmcsrv['debug']['cnameinsrv'], inline=True)
+                embedVar.add_field(name="Animated MOTD", value=bedmcsrv['debug']['animatedmotd'], inline=True)
+                embedVar.add_field(name="Cache Time", value=bedmcsrv['debug']['cachetime'], inline=True)
+                embedVar.add_field(name="HTTP Status (MCSrvStat)", value=bedmcsrv_status_code, inline=True)
                 embedVar.set_thumbnail(url=bedimage_link)
                 await ctx.send(embed=embedVar)
         except Exception as e:
             embedVar = discord.Embed(color=0x607D8B)
-            embedVar.description = f"""
-            Your search has failed. Please try again.\nReason: {e}
-            """
+            embedVar.description = f"Your search has failed. Please try again.\nReason: {e}"
             await ctx.send(embed=embedVar)
-
+            
+    @bedrock.error
+    async def on_message_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embedVar = discord.Embed(color=discord.Color.from_rgb(255, 51, 51))
+            embedVar.description = f"Missing a required argument: {error.param}"
+            msg = await ctx.send(embed=embedVar, delete_after=10)
+            await msg.delete(delay=10)
 
 def setup(bot):
     bot.add_cog(mcsrvstats(bot))
