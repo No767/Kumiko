ea317c98e4ca03c95d9e4815a4668db52f669fa7
No767 <winterpiece104@gmail.com>
2021-11-20 01:03:23 -0800

    Fix the issue with the token refresher once again

--- Bot/Cogs/deviantart-token-refresher.py
+++ Bot/Cogs/deviantart-token-refresher.py
@@ -15,6 +15,7 @@ Client_Secret = os.getenv("DeviantArt_Client_Secret")
 class tokenRefresher(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.refresher.start()
 
     @tasks.loop(minutes=55.0)
     async def refresher(self):
