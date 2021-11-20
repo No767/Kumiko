901b474c55f8d250b211a037578f041559396875
No767 <winterpiece104@gmail.com>
2021-11-19 20:50:59 -0800

    Finish Deviantart Cog's Token Refresher
    This only kicks into effect every 55 mins

--- Bot/Cogs/deviantart-token-refresher.py
+++ Bot/Cogs/deviantart-token-refresher.py
@@ -10,21 +10,26 @@ load_dotenv()
 Refresh_Token = os.getenv("DeviantArt_Refresh_Token")
 Client_ID = os.getenv("DeviantArt_Client_ID")
 Client_Secret = os.getenv("DeviantArt_Client_Secret")
-Auth_Code = os.getenv("DeviantArt_Auth_Code")
 
 
 class tokenRefresher(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @tasks.loop(hours=1)
+    @tasks.loop(minutes=55.0)
     async def refresher(self):
         link = f"https://www.deviantart.com/oauth2/token?client_id={Client_ID}&client_secret={Client_Secret}&grant_type=refresh_token&refresh_token={Refresh_Token}"
         r = requests.get(link)
         data = ujson.loads(r.text)
-        Access_Token = data["access_token"]
-        file = open("../.env", "wr")
-        line = file.readlines()
-        line[32] = f"DeviantArt_Access_Token={Access_Token}\n"
-        writer = file.writelines(line)
-        writer.close()
+        access_token = data['access_token']
+        refresh_token = data['refresh_token']
+        line = open("../.env", "r").readlines()
+        line[34] = f'DeviantArt_Access_Token = "{access_token}"\n'
+        line[35] = f'DeviantArt_Refresh_Token = "{refresh_token}"'
+        file2 = open("../.env", "w+")
+        file2.writelines(line)
+        file2.close()
+
+
+def setup(bot):
+    bot.add_cog(tokenRefresher(bot))
