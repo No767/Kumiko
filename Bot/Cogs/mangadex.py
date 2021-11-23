e19986a0e219bbac6c9ad7876609db84a2f39a3d
No767 <winterpiece104@gmail.com>
2021-11-23 00:41:54 -0800

    Add MangaDex Cog + Make More Cogs Asynchronous
    The MangaDex Cog completely uses AIOHTTP instead of Requests, therefore speeding up the amount of time needed by a lot. This also allows for the MangaDex Reader for Rin to be much much quicker

---
+++ Bot/Cogs/mangadex.py
@@ -0,0 +1,51 @@
+import aiohttp
+import ujson
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+
+class MangadexV1(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(json_serialize=ujson.dumps)
+
+    @commands.command(name="mangadex-search", aliases=["md-search"])
+    async def manga(self, ctx, *, manga:str):
+        params = {"title": f"{manga}"}
+        async with self.session.get(f'https://api.mangadex.org/manga/', params=params) as r:
+            data = await r.json()
+            id = data["data"][0]["id"]
+            async with self.session.get(f'https://api.mangadex.org/manga/{id}?includes["cover_art"]') as resp:
+                md_data = await resp.json()
+                cover_art_id = md_data["data"]["relationships"][2]["id"]
+                embedVar = discord.Embed(title=md_data["data"]["attributes"]["title"]["en"])
+                embedVar.add_field(name="Alt Title", value=md_data["data"]["attributes"]["altTitles"]["en"],
+                                   inline=True)
+                embedVar.add_field(name="Japanese Title", value=md_data["data"]["attributes"]["altTitles"]["ja"],
+                                   inline=True)
+                embedVar.add_field(name="Description",
+                                   value=str(md_data["data"]["attributes"]["description"]["en"]).replace("\n",
+                                                                                                         "").replace(
+                                       "\r", ""), inline=False)
+                embedVar.add_field(name="MAL ID", value=md_data["data"]["attributes"]["links"]["mal_id"], inline=True)
+                embedVar.add_field(name="Original Language", value=md_data["data"]["attributes"]["originalLanguage"],
+                                   inline=True)
+                embedVar.add_field(name="Last Volume", value=md_data["data"]["attributes"]["lastVolume"], inline=True)
+                embedVar.add_field(name="Last Chapter", value=md_data["data"]["attributes"]["lastChapter"], inline=True)
+                embedVar.add_field(name="Publication Demographics",
+                                   value=md_data["data"]["attributes"]["publicationDemographics"], inline=True)
+                embedVar.add_field(name="Status", value=md_data["data"]["attributes"]["status"], inline=True)
+                embedVar.add_field(name="Year", value=md_data["data"]["attributes"]["year"], inline=True)
+                embedVar.add_field(name="Tags", value=[
+                    str(md_data["data"]["attributes"]["tags"]["attributes"]["name"]["en"]).replace("\n", "") for
+                    md_data["data"]["attributes"]["tags"] in md_data["data"]["attributes"]["tags"]], inline=True)
+                embedVar.set_thumbnail(url=f"https://uploads.mangadex.org/covers/{id}/{cover_art_id}.jpg") # this part is currently not working
+                await ctx.send(embed=embedVar)
+                    
+                
+        
+        else:
+            await ctx.send('No manga found.')
--- Bot/Cogs/waifu-pics.py
+++ Bot/Cogs/waifu-pics.py
@@ -1,50 +1,9 @@
 import random
 
 import discord
-import requests
 import ujson
 from discord.ext import commands
-
-
-def anime_pics():
-    waifu_list = [
-        "waifu",
-        "neko",
-        "shinobu",
-        "megumin",
-        "bully",
-        "cuddle",
-        "cry",
-        "hug",
-        "awoo",
-        "kiss",
-        "lick",
-        "pat",
-        "smug",
-        "bonk",
-        "yeet",
-        "blush",
-        "smile",
-        "wave",
-        "highfive",
-        "handhold",
-        "nom",
-        "bite",
-        "glomp",
-        "slap",
-        "kill",
-        "kick",
-        "happy",
-        "wink",
-        "poke",
-        "dance",
-        "cringe",
-    ]
-    searchterm = random.choice(waifu_list)
-    link = f"https://api.waifu.pics/sfw/{searchterm}"
-    r = requests.get(link)
-    return ujson.loads(r.text)
-
+import aiohttp
 
 class waifu(commands.Cog):
     def __init__(self, bot):
@@ -52,15 +11,51 @@ class waifu(commands.Cog):
 
     @commands.command(name="waifupics")
     async def on_messsage(self, ctx):
-        waifu_pics = anime_pics()
-        try:
-            await ctx.send(waifu_pics["url"])
-        except Exception as e:
-            embedVar = discord.Embed()
-            embedVar.description = f"""
-            The query was not successful.\nReason: {e}
-            """
-            await ctx.send(embed=embedVar)
+        waifu_list = [
+            "waifu",
+            "neko",
+            "shinobu",
+            "megumin",
+            "bully",
+            "cuddle",
+            "cry",
+            "hug",
+            "awoo",
+            "kiss",
+            "lick",
+            "pat",
+            "smug",
+            "bonk",
+            "yeet",
+            "blush",
+            "smile",
+            "wave",
+            "highfive",
+            "handhold",
+            "nom",
+            "bite",
+            "glomp",
+            "slap",
+            "kill",
+            "kick",
+            "happy",
+            "wink",
+            "poke",
+            "dance",
+            "cringe",
+        ]
+        searchterm = random.choice(waifu_list)
+        async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
+            async with session.get(f"https://api.waifu.pics/sfw/{searchterm}") as r:
+                waifu_pics = await r.json()
+                try:
+                    await ctx.send(waifu_pics["url"])
+                except Exception as e:
+                    embedVar = discord.Embed()
+                    embedVar.description = f"""
+                    The query was not successful.\nReason: {e}
+                    """
+                    await ctx.send(embed=embedVar)
 
 
 def setup(bot):
