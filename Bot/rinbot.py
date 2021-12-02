3c6ea3172fc814cec8be95431ce8a3f5ae86b7c0
No767 <winterpiece104@gmail.com>
2021-12-01 22:09:54 -0800

    Update Rin to watch for .rinhelp instead

--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -50,7 +50,7 @@ for extension in initial_extensions:
 # Adds in the bot presence
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name=".rinhelp"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".rinhelp"))
 
 
 # Run the bot
