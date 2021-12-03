c08ec7a53a68e617374d9225b87f2c54973a1e62
No767 <winterpiece104@gmail.com>
2021-12-02 23:06:10 -0800

    Rebrand all mentions of Rin into Kumiko

--- Bot/Cogs/kumikoinfo.py
+++ Bot/Cogs/kumikoinfo.py
@@ -7,37 +7,37 @@ class info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="rininfo", help="Server Info")
+    @commands.command(name="kumiko-info", help="Server Info")
     async def on_message(self, message):
         bot = self.bot
         embedVar = discord.Embed(color=14414079)
-        embedVar.set_author(name="Rin Info", icon_url=bot.user.avatar_url)
+        embedVar.set_author(name="Kumiko Info", icon_url=bot.user.avatar_url)
         embedVar.add_field(
             name="About",
-            value="Rin is a discord bot which supports obtaining data from third party services such as DeviantArt, Hypixel, Reddit, MyAnimeList/Jikan, and many others. And you can request such data just from Discord. More services are planned to be supported, such as MangaDex, YT, Twitch, and many others. (The MangaDex service is planned for you to be able to read manga from Discord)",
+            value="Kumiko is a multipurpose bot based off of [Rin](https://github.com/No767/Rin). It has all of the features of Rin, but with more features that are not third-party service related. Planned features includes moderation, and much much more.",
             inline=False,
         )
         embedVar.add_field(
             name="Getting Started",
-            value="To help you get started, type in `.rinhelp` or `.help` in order to access the help page. This will provide you with all of the commands that is available to use as of now.",
+            value="To help you get started, type in `.kumikohelp` or `.help` in order to access the help page. This will provide you with all of the commands that is available to use as of now.",
             inline=False,
         )
         embedVar.add_field(
             name="Questions or Issues?",
-            value="If you have any questions, or any issues, or just an idea that you would like to add, please report them on the [GitHub Issue Tracker](https://github.com/No767/Rin/issues). Note that Rin does not any type of support discord server nor do I plan to make one to begin with.",
+            value="If you have any questions, or any issues, or just an idea that you would like to add, please report them on the [GitHub Issue Tracker](https://github.com/No767/Kumiko/issues). Note that Kumiko does not any type of support discord server nor do I plan to make one to begin with.",
         )
         embedVar.set_footer(
-            text="Project Lead Maintainer and Original Creator of Rin: No767\nOriginal Creator of EasyBot.py and EasyBot Plugins: Chisaku-dev"
+            text="Project Lead Maintainer and Original Creator of Rin and Kumiko: No767\nOriginal Creator of EasyBot.py and EasyBot Plugins: Chisaku-dev"
         )
         await message.channel.send(
             embed=embedVar,
             components=[
                 [
                     Button(label="GitHub",
-                           url="https://github.com/No767/Rin", style=5),
+                           url="https://github.com/No767/Kumiko", style=5),
                     Button(
                         label="Issue Tracker",
-                        url="https://github.com/No767/Rin/issues",
+                        url="https://github.com/No767/Kumiko/issues",
                         style=5,
                     ),
                     Button(
--- Bot/Cogs/version.py
+++ Bot/Cogs/version.py
@@ -9,7 +9,7 @@ class VersionV1(commands.Cog):
     @commands.command(name="version")
     async def version(self, ctx):
         embedVar = discord.Embed()
-        embedVar.description = "Build Version: v1.3.0-dev-b8"
+        embedVar.description = "Build Version: v0.0.1-dev-b1"
         await ctx.send(embed=embedVar)
 
 
