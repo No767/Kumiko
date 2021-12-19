f59684c8df63ae28aeb6853b68ffde34752080c0
No767 <winterpiece104@gmail.com>
2021-12-18 20:24:41 -0800

    Switch to orjson for faster performance + remove more unused libs

--- Bot/Cogs/advice.py
+++ Bot/Cogs/advice.py
@@ -1,7 +1,8 @@
 import aiohttp
 import discord
-import ujson
 from discord.ext import commands
+import ujson
+import orjson
 
 
 class advice_slip(commands.Cog):
@@ -10,14 +11,15 @@ class advice_slip(commands.Cog):
 
     @commands.command(name="advice")
     async def on_message(self, ctx):
-        async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
+        async with aiohttp.ClientSession(json_serialize=orjson.dumps) as session:
             async with session.get("https://api.adviceslip.com/advice") as r:
                 advice_slip = await r.text()
+                advice_slip_formatted = orjson.loads(advice_slip)
                 try:
                     embedVar = discord.Embed(
                         color=discord.Color.from_rgb(251, 204, 255)
                     )
-                    embedVar.description = f"{advice_slip['slip']['advice']}"
+                    embedVar.description = f"{advice_slip_formatted['slip']['advice']}"
                     embedVar.set_footer(
                         text=f"Requested by {ctx.message.author.name}",
                         icon_url=ctx.message.author.avatar_url,
