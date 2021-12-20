f5e357f59512357537ba914478f864d635bdd0e8
No767 <winterpiece104@gmail.com>
2021-12-20 12:37:57 -0800

    Make Jikan/MAL Cog Async + Attempted Pagination for MD Cog

--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -38,8 +38,8 @@ initial_extensions = [
     "Cogs.rininvite",
     "Cogs.version",
     "Cogs.clear",
-    "Cogs.deviantart-token-refresher",
-    "Cogs.deviantart",
+    'Cogs.mangadex',
+    "Cogs.button-test",
 ]
 for extension in initial_extensions:
     bot.load_extension(extension)
