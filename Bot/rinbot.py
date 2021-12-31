6435a2f3e59e68fb3b806ab20530e5b0bca67ec9
No767 <winterpiece104@gmail.com>
2021-12-30 20:14:45 -0800

    Get Rin Ready for Prod

--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Grabs the bot's token from the .env file
 load_dotenv()
-TOKEN = os.getenv("Hanako_Token")
+TOKEN = os.getenv("TOKEN")
 intents = Intents.all()
 bot = commands.Bot(command_prefix=".", help_command=None)
 
@@ -41,6 +41,8 @@ initial_extensions = [
     "Cogs.youtube",
     "Cogs.jisho",
     "Cogs.disquest",
+    "Cogs.deviantart-token-refresher",
+    "Cogs.deviantart"
 ]
 for extension in initial_extensions:
     bot.load_extension(extension)
