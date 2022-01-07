26b30d0aa2f1e9feee7e63d602d627ff332cf6e1
No767 <73260931+No767@users.noreply.github.com>
2022-01-06 23:43:03 -0800

    Use MongoDB for Eco + Finish Bonk Cmd

---
+++ Bot/Cogs/bonk.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class BonkV1(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="bonk")
+    async def bonk_user(self, ctx):
+        embedVar = discord.Embed()
+        embedVar.description = f"Bonked {ctx.author.mention}"
+        file = discord.File("./Bot/Cogs/images/bonk.gif")
+        embedVar.set_image(url="attachment://bonk.gif")
+        await ctx.send(embed=embedVar, file=file)
+        
+def setup(bot):
+    bot.add_cog(BonkV1(bot))
