5f5294fcd98d3f34b35cd7e8386e979eee2f0529
No767 <73260931+No767@users.noreply.github.com>
2022-01-08 09:46:31 -0800

    Make Waifu Generator Cog Async
    Should speed up performance

--- Bot/Cogs/waifu-generator.py
+++ Bot/Cogs/waifu-generator.py
@@ -3,7 +3,8 @@ import discord
 import requests
 from discord.ext import commands
 from fake_useragent import UserAgent
-
+import aiohttp
+import orjson
 
 class waifu(commands.Cog):
     def __init__(self, bot):
@@ -11,22 +12,22 @@ class waifu(commands.Cog):
 
     @commands.command(name="waifu")
     async def on_message(self, ctx):
-        headers = {"UserAgent": UserAgent().random}
-        URL = "https://www.mywaifulist.moe/random"
-
-        soup = bs4.BeautifulSoup(requests.get(
-            URL, headers=headers).text, "html.parser")
-        waifu_title = soup.find(
-            "meta", attrs={"property": "og:title"}).attrs["content"]
-        image_url = soup.find(
-            "meta", attrs={"property": "og:image"}).attrs["content"]
-        description = soup.find("p", id="description").get_text()
-        embedVar = discord.Embed(
-            title=waifu_title, color=discord.Color.from_rgb(208, 189, 255)
-        )
-        embedVar.description = f"{description}"
-        embedVar.set_image(url=image_url)
-        await ctx.send(embed=embedVar)
+        async with aiohttp.ClientSession(json_serialize=orjson.dumps) as session:
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"}
+            async with session.get("https://www.mywaifulist.moe/random", headers=headers) as r:
+                data = await r.text()
+                soup = bs4.BeautifulSoup(data, "html.parser")
+                waifu_title = soup.find(
+                    "meta", attrs={"property": "og:title"}).attrs["content"]
+                image_url = soup.find(
+                    "meta", attrs={"property": "og:image"}).attrs["content"]
+                description = soup.find("p", id="description").get_text()
+                embedVar = discord.Embed(
+                    title=waifu_title, color=discord.Color.from_rgb(208, 189, 255)
+                )
+                embedVar.description = f"{description}"
+                embedVar.set_image(url=image_url)
+                await ctx.send(embed=embedVar)
 
 
 def setup(bot):
