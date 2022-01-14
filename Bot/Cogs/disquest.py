cd34e6508df8dac9bb675193b61a63483ee2ef1c
No767 <73260931+No767@users.noreply.github.com>
2022-01-13 23:55:35 -0800

    Make DisQuest Async
    Going to use Asyncpg instead, so this should be a lot faster now...

--- Bot/Cogs/disquest.py
+++ Bot/Cogs/disquest.py
@@ -7,6 +7,9 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from sqlalchemy import (BigInteger, Column, Integer, MetaData, Sequence, Table,
                         create_engine, func, select)
+from sqlalchemy.ext.asyncio import create_async_engine
+import asyncio
+import uvloop
 
 load_dotenv()
 
@@ -20,11 +23,9 @@ class disaccount:
         self.id = ctx.author.id
         self.gid = ctx.guild.id
 
-    def getxp(self):
+    async def getxp(self):
         meta = MetaData()
-        engine = create_engine(
-            f"postgresql+psycopg2://{Username}:{Password}@{IP}:5432/rin-disquest"
-        )
+        engine = create_async_engine(f'postgresql+asyncpg://{Username}:{Password}@{IP}:5432/rin-disquest')
         users = Table(
             "rin-users-v4",
             meta,
@@ -39,23 +40,22 @@ class disaccount:
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = select(users.c.xp).where(
-            users.c.id == self.id, users.c.gid == self.gid)
-        results = conn.execute(s).fetchone()
-        if results is None:
-            insert_new = users.insert().values(xp=0, id=self.id, gid=self.gid)
-            conn.execute(insert_new)
-        else:
-            for row in results:
-                return row
-        conn.close()
+        async with engine.connect() as conn:
+            s = select(users.c.xp).where(
+                users.c.id == self.id, users.c.gid == self.gid)
+            results = await conn.execute(s)
+            results_fetched = results.fetchone()
+            if results_fetched is None:
+                insert_new = users.insert().values(xp=0, id=self.id, gid=self.gid)
+                await conn.execute(insert_new)
+            else:
+                for row in results_fetched:
+                    return row
+            await conn.close()
 
-    def setxp(self, xp):
+    async def setxp(self, xp):
         meta = MetaData()
-        engine = create_engine(
-            f"postgresql+psycopg2://{Username}:{Password}@{IP}:5432/rin-disquest"
-        )
+        engine = create_async_engine(f"postgresql+asyncpg://{Username}:{Password}@{IP}:5432/rin-disquest")
         users = Table(
             "rin-users-v4",
             meta,
@@ -70,20 +70,20 @@ class disaccount:
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        update_values = (
-            users.update()
-            .values(xp=xp)
-            .filter(users.c.id == self.id)
-            .filter(users.c.gid == self.gid)
-        )
-        conn.execute(update_values)
-        conn.close()
+        async with engine.connect() as conn:
+            update_values = (
+                users.update()
+                .values(xp=xp)
+                .filter(users.c.id == self.id)
+                .filter(users.c.gid == self.gid)
+            )
+            await conn.execute(update_values)
+            await conn.close()
 
     def addxp(self, offset):
-        pxp = self.getxp()
+        pxp = await self.getxp()
         pxp += offset
-        self.setxp(pxp)
+        await self.setxp(pxp)
 
 
 class lvl:
@@ -107,7 +107,7 @@ class DisQuest(commands.Cog):
     )
     async def mylvl(self, ctx):
         user = disaccount(ctx)
-        xp = user.getxp()
+        xp = await user.getxp()
         embedVar = discord.Embed(color=discord.Color.from_rgb(255, 217, 254))
         embedVar.add_field(
             name="User", value=f"{ctx.author.mention}", inline=True)
@@ -115,6 +115,8 @@ class DisQuest(commands.Cog):
         embedVar.add_field(
             name="XP", value=f"{xp}/{lvl.next(xp)*100}", inline=True)
         await ctx.send(embed=embedVar)
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 class DisQuestV2(commands.Cog):
@@ -127,9 +129,7 @@ class DisQuestV2(commands.Cog):
     async def rank(self, ctx):
         gid = ctx.guild.id
         meta = MetaData()
-        engine = create_engine(
-            f"postgresql+psycopg2://{Username}:{Password}@{IP}:5432/rin-disquest"
-        )
+        engine = create_async_engine(f"postgresql+asyncpg://{Username}:{Password}@{IP}:5432/rin-disquest")
         users = Table(
             "rin-users-v4",
             meta,
@@ -144,21 +144,23 @@ class DisQuestV2(commands.Cog):
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = (
-            select(Column("id", BigInteger), Column("xp", Integer))
-            .where(users.c.gid == gid)
-            .order_by(users.c.xp.desc())
-        )
-        results = conn.execute(s)
-        members = list(results.fetchall())
-        for i, mem in enumerate(members):
-            members[
-                i
-            ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
-        embedVar = discord.Embed(color=discord.Color.from_rgb(254, 255, 217))
-        embedVar.description = f"**Server Rankings**\n{''.join(members)}"
-        await ctx.send(embed=embedVar)
+        async with engine.connect() as conn:
+            s = (
+                select(Column("id", BigInteger), Column("xp", Integer))
+                .where(users.c.gid == gid)
+                .order_by(users.c.xp.desc())
+            )
+            results = await conn.execute(s)
+            members = list(await results.fetchall())
+            for i, mem in enumerate(members):
+                members[
+                    i
+                ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
+            embedVar = discord.Embed(color=discord.Color.from_rgb(254, 255, 217))
+            embedVar.description = f"**Server Rankings**\n{''.join(members)}"
+            await ctx.send(embed=embedVar)
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 class DisQuestV3(commands.Cog):
@@ -172,9 +174,7 @@ class DisQuestV3(commands.Cog):
     )
     async def grank(self, ctx):
         meta = MetaData()
-        engine = create_engine(
-            f"postgresql+psycopg2://{Username}:{Password}@{IP}:5432/rin-disquest"
-        )
+        engine = create_async_engine(f"postgresql+asyncpg://{Username}:{Password}@{IP}:5432/rin-disquest")
         users = Table(
             "rin-users-v4",
             meta,
@@ -189,23 +189,26 @@ class DisQuestV3(commands.Cog):
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = (
-            select(Column("id", Integer), func.sum(users.c.xp).label("txp"))
-            .group_by(users.c.id)
-            .group_by(users.c.xp)
-            .order_by(users.c.xp.desc())
-            .limit(10)
-        )
-        results = conn.execute(s).fetchall()
-        members = list(results)
-        for i, mem in enumerate(members):
-            members[
-                i
-            ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
-        embedVar = discord.Embed(color=discord.Color.from_rgb(217, 255, 251))
-        embedVar.description = f"**Global Rankings**\n{''.join(members)}"
-        await ctx.send(embed=embedVar)
+        async with engine.connect() as conn:
+            s = (
+                select(Column("id", Integer), func.sum(users.c.xp).label("txp"))
+                .group_by(users.c.id)
+                .group_by(users.c.xp)
+                .order_by(users.c.xp.desc())
+                .limit(10)
+            )
+            results = await conn.execute(s)
+            results_fetched = await results.fetchall()
+            members = list(results_fetched)
+            for i, mem in enumerate(members):
+                members[
+                    i
+                ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
+            embedVar = discord.Embed(color=discord.Color.from_rgb(217, 255, 251))
+            embedVar.description = f"**Global Rankings**\n{''.join(members)}"
+            await ctx.send(embed=embedVar)
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 class DisQuestV4(commands.Cog):
@@ -219,6 +222,8 @@ class DisQuestV4(commands.Cog):
         user = disaccount(ctx)
         reward = random.randint(0, 20)
         user.addxp(reward)
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 def setup(bot):
