3014ecf9faf505cc6c8bf9d8d5ffc902087d530a
No767 <73260931+No767@users.noreply.github.com>
2022-02-05 00:17:56 -0800

    Fix Merge Conflict Issues

--- Bot/Cogs/waifu-pics.py
+++ Bot/Cogs/waifu-pics.py
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-import asyncio
-import random
-
-import aiohttp
-import discord
-import orjson
-import uvloop
-from discord.ext import commands
-
-
-class waifu(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(name="waifupics", aliases=["wp"])
-    async def on_messsage(self, ctx):
-        waifu_list = [
-            "waifu",
-            "neko",
-            "shinobu",
-            "megumin",
-            "bully",
-            "cuddle",
-            "cry",
-            "hug",
-            "awoo",
-            "kiss",
-            "lick",
-            "pat",
-            "smug",
-            "bonk",
-            "yeet",
-            "blush",
-            "smile",
-            "wave",
-            "highfive",
-            "handhold",
-            "nom",
-            "bite",
-            "glomp",
-            "slap",
-            "kill",
-            "kick",
-            "happy",
-            "wink",
-            "poke",
-            "dance",
-            "cringe",
-        ]
-        searchterm = random.choice(waifu_list)
-        async with aiohttp.ClientSession(json_serialize=orjson.dumps) as session:
-            async with session.get(f"https://api.waifu.pics/sfw/{searchterm}") as r:
-                waifu_pics = await r.json()
-                try:
-                    await ctx.send(waifu_pics["url"])
-                except Exception as e:
-                    embedVar = discord.Embed()
-                    embedVar.description = "The query was not successful"
-                    embedVar.add_field(name="Reason", value=e, inline=True)
-                    await ctx.send(embed=embedVar)
-
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
-
-def setup(bot):
-    bot.add_cog(waifu(bot))
-=======
 import asyncio
 import random
 
@@ -132,4 +64,3 @@ class waifuPics(commands.Cog):
 
 def setup(bot):
     bot.add_cog(waifuPics(bot))
->>>>>>> 0a819321458273f6768c2ac2ea12c4208edea6dd
