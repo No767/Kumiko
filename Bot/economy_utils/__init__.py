6ff766e3dd0241a345b76b0061d30f614da5fb37
No767 <73260931+No767@users.noreply.github.com>
2022-05-09 00:34:34 -0700

    Add Basic Inv System
    Now when users purchase an item from the marketplace, it will get stored, so the user can take a look at their inv

--- Bot/economy_utils/__init__.py
+++ Bot/economy_utils/__init__.py
@@ -1,4 +1,5 @@
 from .eco_main import KumikoEcoUtils
 from .eco_user import KumikoEcoUserUtils
+from .user_inv import UsersInv
 
-__all__ = ["KumikoEcoUtils", "KumikoEcoUserUtils"]
+__all__ = ["KumikoEcoUtils", "KumikoEcoUserUtils", "UsersInv"]
---
+++ Bot/economy_utils/user_inv.py
@@ -0,0 +1,42 @@
+import asyncio
+import os
+from typing import Optional
+
+import motor.motor_asyncio
+import uvloop
+from beanie import Document, init_beanie
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+MongoDB_Password = os.getenv("MongoDB_Password")
+Username = os.getenv("MongoDB_Username")
+Server_IP = os.getenv("MongoDB_Server_IP")
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    amount: int
+
+
+class User_Inventory(Document):
+    user_id: int
+    items: Item
+
+
+class UsersInv:
+    def __init__(self):
+        self.self = self
+
+    async def insItem(self, user_id: int, items: dict):
+        client = motor.motor_asyncio.AsyncIOMotorClient(
+            f"mongodb://{Username}:{MongoDB_Password}@{Server_IP}:27017"
+        )
+        await init_beanie(
+            database=client.kumiko_marketplace, document_models=[User_Inventory]
+        )
+        entry = User_Inventory(user_id=user_id, items=items)
+        await entry.create()
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
