8270a70067920317b2931336d22de4ee99f85a07
No767 <73260931+No767@users.noreply.github.com>
2022-07-01 02:56:00 -0700

    Adjust Postgres-Init Script

--- scripts/postgres-init.py
+++ scripts/postgres-init.py
@@ -1,6 +1,10 @@
 import asyncio
+import sys
 
 import uvloop
+
+# very hacky solution, probably will not work...
+sys.path.insert(0, "/mnt/d/GitHub Repos [Local]/Kumiko/Bot/")
 
 from Bot.economy_utils import KumikoEcoUserUtils
 
@@ -8,7 +12,7 @@ utils = KumikoEcoUserUtils()
 
 
 async def main():
-    await utils.initTables()
+    await utils.getUser(454357482102587393)
 
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
