5a413b85b8a549077c3635c4fd57c5f9403b5a6f
No767 <73260931+No767@users.noreply.github.com>
2022-07-02 01:11:05 -0700

    Fix Postgres-Init Script
    The path wasn't working, but finally fixed it

--- scripts/postgres-init.py
+++ scripts/postgres-init.py
@@ -1,18 +1,19 @@
 import asyncio
+import os
 import sys
 
 import uvloop
 
-# very hacky solution, probably will not work...
-sys.path.insert(0, "/mnt/d/GitHub Repos [Local]/Kumiko/Bot/")
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "Bot"))
 
-from Bot.economy_utils import KumikoEcoUserUtils
+from economy_utils import KumikoEcoUserUtils
 
 utils = KumikoEcoUserUtils()
 
 
 async def main():
-    await utils.getUser(454357482102587393)
+    await utils.initTables()
+    await utils.initInvTables()
 
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
