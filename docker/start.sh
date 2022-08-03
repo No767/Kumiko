c7fe20d327ac346a164def1906d6d5166098c9b6
Ellie <me@silkky.dev>
2022-08-03 18:48:50 +1000

    Use echo instead of sed for API environment values

--- docker/start.sh
+++ docker/start.sh
@@ -15,79 +15,79 @@ fi
 # API Keys
 # Blue Alliance
 if [[ -v BLUE_ALLIANCE_API_KEY ]]; then
-    sed -i "s@blue_alliance_api_key@$BLUE_ALLIANCE_API_KEY@g" /Rin/Bot/.env
+    echo "Blue_Alliance_API_Key=${BLUE_ALLIANCE_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing Blue Alliance API key! BLUE_ALLIANCE_API_KEY environment variable is not set."
 fi 
 # Discord
 if [[ -v DISCORD_BOTS_API_KEY ]]; then
-    sed -i "s@discord_bots_api_key@$DISCORD_BOTS_API_KEY@g" /Rin/Bot/.env
+    echo "Discord_Bots_API_Key=${DISCORD_BOTS_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing Discord Bots API key! DISCORD_BOTS_API_KEY environment variable is not set."
 fi 
 # First Events
 if [[ -v FIRST_EVENTS_FINAL_KEY ]]; then
-    sed -i "s@first_events_final_key@$FIRST_EVENTS_FINAL_KEY@g" /Rin/Bot/.env
+    echo "FIRST_Events_Final_Key=${FIRST_EVENTS_FINAL_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing First Events Final key! FIRST_EVENTS_FINAL_KEY environment variable is not set."
 fi 
 # GitHub
 if [[ -v GITHUB_API_ACCESS_TOKEN ]]; then
-    sed -i "s@github_api_access_token@$GITHUB_API_ACCESS_TOKEN@g" /Rin/Bot/.env
+    echo "GitHub_API_Access_Token=${GITHUB_API_ACCESS_TOKEN}" >> /Rin/Bot/.env
 else
     echo "Missing GitHub API token! GITHUB_API_ACCESS_TOKEN environment variable is not set."
 fi 
 # Hypixel
 if [[ -v HYPIXEL_API_KEY ]]; then
-    sed -i "s@hypixel_api_key@$HYPIXEL_API_KEY@g" /Rin/Bot/.env
+    echo "Hypixel_API_Key=${HYPIXEL_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing Hypixel API key! HYPIXEL_API_KEY environment variable is not set."
 fi 
 # Reddit ID
 if [[ -v REDDIT_ID ]]; then
-    sed -i "s@reddit_id@$REDDIT_ID@g" /Rin/Bot/.env
+    echo "Reddit_ID=${REDDIT_ID}" >> /Rin/Bot/.env
 else
     echo "Missing Reddit ID! REDDIT_ID environment variable is not set."
 fi 
 # Reddit Secret
 if [[ -v REDDIT_SECRET ]]; then
-    sed -i "s@reddit_secret@$REDDIT_SECRET@g" /Rin/Bot/.env
+    echo "Reddit_Secret=${REDDIT_SECRET}" >> /Rin/Bot/.env
 else
     echo "Missing Reddit secret! REDDIT_SECRET environment variable is not set."
 fi 
 # Tenor
 if [[ -v TENOR_API_KEY ]]; then
-    sed -i "s@tenor_api_key@$TENOR_API_KEY@g" /Rin/Bot/.env
+    echo "Tenor_API_V2_Key=${TENOR_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing Tenor API key! TENOR_API_KEY environment variable is not set."
 fi 
 # Top GG
 if [[ -v TOP_GG_API_KEY ]]; then
-    sed -i "s@top_gg_api_key@$TOP_GG_API_KEY@g" /Rin/Bot/.env
+    echo "Top_GG_API_Key=${TOP_GG_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing Top GG API key! TOP_GG_API_KEY environment variable is not set."
 fi
 # Twitch access token
 if [[ -v TWITCH_API_ACCESS_TOKEN ]]; then
-    sed -i "s@twitch_api_access_token@$TWITCH_API_ACCESS_TOKEN@g" /Rin/Bot/.env
+    echo "Twitch_API_Access_Token=${TWITCH_API_ACCESS_TOKEN}" >> /Rin/Bot/.env
 else
     echo "Missing Twitch API access token! TWITCH_API_ACCESS_TOKEN environment variable is not set."
 fi
 # Twitch client ID
 if [[ -v TWITCH_API_CLIENT_ID ]]; then
-    sed -i "s@twitch_api_client_id@$TWITCH_API_CLIENT_ID@g" /Rin/Bot/.env
+    echo "Twitch_API_Client_ID=${TWITCH_API_CLIENT_ID}" >> /Rin/Bot/.env
 else
     echo "Missing Twitch API client id! TWITCH_API_CLIENT_ID environment variable is not set."
 fi
 # Twitter
 if [[ -v TWITTER_BEARER_TOKEN ]]; then
-    sed -i "s@twitter_bearer_token@$TWITTER_BEARER_TOKEN@g" /Rin/Bot/.env
+    echo "Twitter_Bearer_Token=${TWITTER_BEARER_TOKEN}" >> /Rin/Bot/.env
 else
     echo "Missing Twitter bearer token! TWITTER_BEARER_TOKEN environment variable is not set."
 fi
 # YouTube
 if [[ -v YOUTUBE_API_KEY ]]; then
-    sed -i "s@youtube_api_key@$YOUTUBE_API_KEY@g" /Rin/Bot/.env
+    echo "YouTube_API_Key=${YOUTUBE_API_KEY}" >> /Rin/Bot/.env
 else
     echo "Missing YouTube API key! YOUTUBE_API_KEY environment variable is not set."
 fi
