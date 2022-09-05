8a7ec5b7788f2ea7d897a8e9fcb26decd3d742aa
No767 <73260931+No767@users.noreply.github.com>
2022-09-05 01:52:06 -0700

    Make Avatar Cmd Accept discord.Member instead of str

--- Bot/Cogs/avatar.py
+++ Bot/Cogs/avatar.py
@@ -1,5 +1,3 @@
-import re
-
 import discord
 from discord.commands import Option, SlashCommandGroup
 from discord.ext import commands
@@ -18,20 +16,19 @@ class UserAvatar(commands.Cog):
 
     @avatarGet.command(name="user")
     async def avatarGetUser(
-        self, ctx, *, user: Option(str, "The username you wish to get")
+        self, ctx, *, user: Option(discord.Member, "The username you wish to get")
     ):
         """Obtains the given user's avatar"""
         try:
-            parsedUserID = int(re.sub("[^a-zA-Z0-9 ]", "", user))
-            getUserInfo = await self.bot.fetch_user(parsedUserID)
             embed = discord.Embed()
-            embed.title = f"{getUserInfo.name}#{getUserInfo.discriminator}"
-            embed.set_image(url=getUserInfo.display_avatar.url)
+            embed.title = f"{user.display_name}#{user.discriminator}'s avatar"
+            embed.set_image(url=user.display_avatar.url)
             await ctx.respond(embed=embed)
-        except Exception:
+        except Exception as e:
             await ctx.respond(
                 embed=discord.Embed(
-                    description="Oops, it looks like you may have not put in an actual discord username. Please try again."
+                    title="Oops, an error occurred!",
+                    description=f"{type(e).__name__}: {str(e)}",
                 )
             )
 
