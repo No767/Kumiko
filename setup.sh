5051d90840c47634c31ac08ba77e7acf2c8d6cf9
No767 <73260931+No767@users.noreply.github.com>
2022-09-27 10:26:34 -0700

    Use custom image for Postgres Docker Compose instead

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 mkdir ~/.kumiko
-cd ~/.kumiko && mkdir Postgres-Docker
-cd Postgres-Docker && wget -q https://raw.githubusercontent.com/No767/Kumiko/dev/Postgres-Docker/Dockerfile \
-&& wget -q https://raw.githubusercontent.com/No767/Kumiko/dev/Postgres-Docker/init.sh \
-&& wget -q https://raw.githubusercontent.com/No767/Kumiko/dev/Postgres-Docker/ws-data-init.sh \
-&& wget -q https://raw.githubusercontent.com/No767/Kumiko/dev/Postgres-Docker/ws_data.csv
-cd .. && wget -O docker-compose.yml -q https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
+cd ~/.kumiko &&  wget -O docker-compose.yml -q https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
 && wget -O .env -q https://raw.githubusercontent.com/No767/Kumiko/dev/.env-docker-example
 echo "Downloaded and set up all the files needed for Kumiko!"
