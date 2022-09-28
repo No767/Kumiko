123ae1a651ee52f951aaf22b38b43871dc7945f5
No767 <73260931+No767@users.noreply.github.com>
2022-09-27 18:22:05 -0700

    [skip ci] Issue some fixes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-curl -s -o docker-compose.yml -q https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
-&& curl -s -o .env -q https://raw.githubusercontent.com/No767/Kumiko/dev/.env-docker-example
+curl -so docker-compose.yml https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
+&& curl -so .env https://raw.githubusercontent.com/No767/Kumiko/dev/.env-docker-example
 echo "Downloaded and set up all the files needed for Kumiko!"
