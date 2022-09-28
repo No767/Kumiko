d2229029c85d5b1ddadd477022d2fd2381357fe0
No767 <73260931+No767@users.noreply.github.com>
2022-09-27 18:13:52 -0700

    [skip ci] Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-mkdir ~/.kumiko
-cd ~/.kumiko &&  wget -O docker-compose.yml -q https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
+wget -O docker-compose.yml -q https://raw.githubusercontent.com/No767/Kumiko/dev/docker-compose-example.yml \
 && wget -O .env -q https://raw.githubusercontent.com/No767/Kumiko/dev/.env-docker-example
 echo "Downloaded and set up all the files needed for Kumiko!"
