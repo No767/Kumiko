2474653f26247099884491387fc3b00ba535585c
No767 <73260931+No767@users.noreply.github.com>
2022-09-29 23:57:36 -0700

    Finish up Models for Admin Logs Utils

--- Bot/admin_logs_utils/models.py
+++ Bot/admin_logs_utils/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, Column, String, Text
+from sqlalchemy import BigInteger, Boolean, Column, String, Text
 
 from .db import Base
 
@@ -8,9 +8,28 @@ class AdminLogs(Base):
 
     uuid = Column(String, primary_key=True)
     guild_id = Column(BigInteger)
-    action_user = Column(BigInteger)
+    action_user_id = Column(BigInteger)
+    user_affected_id = Column(BigInteger)
     type_of_action = Column(String)
     title = Column(String)
     reason = Column(Text)
     date_issued = Column(String)
     date_resolved = Column(String)
+    duration = Column(BigInteger)
+    resolved = Column(Boolean)
+
+    def __iter__(self):
+        yield "uuid", self.uuid
+        yield "guild_id", self.guild_id
+        yield "action_user_id", self.action_user_id
+        yield "user_affected_id", self.user_affected_id
+        yield "type_of_action", self.type_of_actios
+        yield "title", self.title
+        yield "reason", self.reason
+        yield "date_issued", self.date_issued
+        yield "date_resolved", self.date_resolved
+        yield "duration", self.duration
+        yield "resolved", self.resolved
+
+    def __repr__(self):
+        return f"AdminLogs(uuid={self.uuid!r}, guild_id={self.guild_id!r}, action_user_id={self.action_user_id!r}, user_affected_id={self.user_affected_id!r}, type_of_action={self.type_of_action!r}, title={self.title!r}, reason={self.reason!r}, date_issued={self.date_issued!r}, date_resolved={self.date_resolved!r}, duration={self.duration!r}, resolved={self.resolved!r})"
