e9e136e50cc7b3368754edd74a836f96b5cbd59f
No767 <73260931+No767@users.noreply.github.com>
2022-11-11 21:06:38 -0800

    Subclass Kumiko

--- Bot/kumikobot.py
+++ Bot/kumikobot.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import discord
 import uvloop
 from dotenv import load_dotenv
+from kumikocore import KumikoCore
 
 # Grabs the bot's token from the .env file
 load_dotenv()
@@ -14,7 +15,13 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("Dev_Bot_Token")
 intents = discord.Intents.default()
 intents.members = True
-bot = discord.Bot(intents=intents)
+
+path = Path(__file__).parents[0].absolute()
+cogsPath = os.path.join(str(path), "Cogs")
+libsPath = os.path.join(str(path), "Libs")
+sys.path.append(libsPath)
+
+bot = KumikoCore(intents=intents)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -25,43 +32,6 @@ logging.basicConfig(
 # Literally prevent these modules from attempting to log info stuff
 logging.getLogger("asyncio_redis").setLevel(logging.WARNING)
 logging.getLogger("gql").setLevel(logging.WARNING)
-
-# New system for loading all cogs
-path = Path(__file__).parents[0].absolute()
-cogsPath = os.path.join(str(path), "Cogs")
-libsPath = os.path.join(str(path), "Libs")
-sys.path.append(libsPath)
-
-cogsList = os.listdir(cogsPath)
-
-for cogDir in cogsList:
-    if cogDir not in ["__pycache__"]:
-        subCogsList = os.listdir(os.path.join(cogsPath, cogDir))
-        for subCogDir in subCogsList:
-            if subCogDir not in [
-                "__pycache__",
-            ] and not subCogDir.endswith(".py"):
-                for lastCog in os.listdir(os.path.join(cogsPath, cogDir, subCogDir)):
-                    if lastCog not in ["__pycache__", "config"]:
-                        logging.debug(
-                            f"Loaded Cog: Cogs.{cogDir}.{subCogDir}.{lastCog[:-3]}"
-                        )
-                        bot.load_extension(
-                            f"Cogs.{cogDir}.{subCogDir}.{lastCog[:-3]}", store=False
-                        )
-            elif subCogDir.endswith(".py"):
-                logging.debug(f"Loaded Cog: Cogs.{cogDir}.{subCogDir[:-3]}")
-                bot.load_extension(f"Cogs.{cogDir}.{subCogDir[:-3]}", store=False)
-
-# Adds in the bot presence
-@bot.event
-async def on_ready():
-    logging.info(f"Logged in as {bot.user.name}")
-    logging.info(f"{bot.user.name} is ready to go! All checkers are loaded and ready!")
-    await bot.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="/help")
-    )
-
 
 # Run the bot
 if __name__ == "__main__":
---
+++ Bot/kumikocore.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import sys
+from pathlib import Path
+
+import discord
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] | %(asctime)s >> %(message)s",
+    datefmt="[%m/%d/%Y] [%I:%M:%S %p %Z]",
+)
+
+logging.getLogger("asyncio_redis").setLevel(logging.WARNING)
+logging.getLogger("gql").setLevel(logging.WARNING)
+
+path = Path(__file__).parents[0].absolute()
+cogsPath = os.path.join(str(path), "Cogs")
+libsPath = os.path.join(str(path), "Libs")
+sys.path.append(libsPath)
+
+
+class KumikoCore(discord.Bot):
+    """The core of Kumiko - Subclassed this time"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.load_cogs()
+
+    def load_cogs(self):
+        """Kumiko's system to load cogs"""
+        cogsList = os.listdir(cogsPath)
+        for cogDir in cogsList:
+            if cogDir not in ["__pycache__"]:
+                subCogsList = os.listdir(os.path.join(cogsPath, cogDir))
+                for subCogDir in subCogsList:
+                    if subCogDir not in [
+                        "__pycache__",
+                    ] and not subCogDir.endswith(".py"):
+                        for lastCog in os.listdir(
+                            os.path.join(cogsPath, cogDir, subCogDir)
+                        ):
+                            if lastCog not in ["__pycache__", "config"]:
+                                logging.debug(
+                                    f"Loaded Cog: Cogs.{cogDir}.{subCogDir}.{lastCog[:-3]}"
+                                )
+                                self.load_extension(
+                                    f"Cogs.{cogDir}.{subCogDir}.{lastCog[:-3]}",
+                                    store=False,
+                                )
+                    elif subCogDir.endswith(".py"):
+                        logging.debug(f"Loaded Cog: Cogs.{cogDir}.{subCogDir[:-3]}")
+                        self.load_extension(
+                            f"Cogs.{cogDir}.{subCogDir[:-3]}", store=False
+                        )
+
+    async def on_ready(self):
+        logging.info(f"Logged in as {self.user.name}")
+        logging.info(
+            f"{self.user.name} is ready to go! All checkers are loaded and ready!"
+        )
+        await self.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.watching, name="/help")
+        )
