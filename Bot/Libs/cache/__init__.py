c80134e0e54007717c357145765ecb79b9d0759e
No767 <73260931+No767@users.noreply.github.com>
2023-03-04 23:17:53 -0800

    Fix cache package issues

--- Bot/Libs/cache/__init__.py
+++ Bot/Libs/cache/__init__.py
@@ -1,6 +1,13 @@
+from .decorators import cached
 from .global_cache import setupMemCacheBuiltin
 from .key_builder import CommandKeyBuilder
 from .mem_cache import MemoryCache
 from .redis_cache import KumikoCache
 
-__all__ = ["setupMemCacheBuiltin", "MemoryCache", "CommandKeyBuilder", "KumikoCache"]
+__all__ = [
+    "setupMemCacheBuiltin",
+    "MemoryCache",
+    "CommandKeyBuilder",
+    "KumikoCache",
+    "cached",
+]
