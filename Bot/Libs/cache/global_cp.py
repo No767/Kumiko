a6338cf79095a33aa81113d0269b8892e290d38d
No767 <73260931+No767@users.noreply.github.com>
2023-04-02 01:48:30 -0700

    Pass in Redis Password to KumikoCPM

--- Bot/Libs/cache/global_cp.py
+++ Bot/Libs/cache/global_cp.py
@@ -10,6 +10,8 @@ load_dotenv(dotenv_path=path)
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
-REDIS_PASSWORD = os.environ["REDIS_PASSWORD"]
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
-kumikoCP: KumikoCPManager = KumikoCPManager(host=REDIS_HOST, port=int(REDIS_PORT))
+kumikoCP: KumikoCPManager = KumikoCPManager(
+    host=REDIS_HOST, port=int(REDIS_PORT), password=REDIS_PASSWORD
+)
