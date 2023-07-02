31c4a0756c89153d8566015b0efe1c5607f9056c
Noelle <73260931+No767@users.noreply.github.com>
2023-07-02 01:00:16 -0700

    Fix cirular imports issue

--- Bot/Libs/utils/prefix.py
+++ Bot/Libs/utils/prefix.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
-from kumikocore import KumikoCore
 
 
-async def get_prefix(bot: KumikoCore, msg: discord.Message):
+# removed the type hinting bc of circular imports
+# if there is a way to solve it, then it would be back
+async def get_prefix(bot, msg: discord.Message):
     if msg.guild is None:
         return commands.when_mentioned_or(bot.default_prefix)(bot, msg)
     try:
@@ -12,7 +13,7 @@ async def get_prefix(bot: KumikoCore, msg: discord.Message):
         # if not in the LRU cache
         async with bot.pool.acquire() as conn:
             query = """
-            SELECT prefix FROM guilds WHERE id = $1;
+            SELECT prefix FROM guild WHERE id = $1;
             """
             updateQuery = """
             UPDATE guild
