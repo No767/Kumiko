b3e836c2aaa08925250025c880b8fde0723981fc
No767 <73260931+No767@users.noreply.github.com>
2023-09-29 20:40:15 -0700

    Use recommended ruff rules

--- Bot/Libs/utils/timer/fuzzy_utils.py
+++ Bot/Libs/utils/timer/fuzzy_utils.py
@@ -152,7 +152,10 @@ def extract(
     limit: Optional[int] = 10,
 ) -> list[tuple[str, int]] | list[tuple[str, int, T]]:
     it = _extraction_generator(query, choices, scorer, score_cutoff)
-    key = lambda t: t[1]
+
+    def key(t):
+        return t[1]
+
     if limit is not None:
         return heapq.nlargest(limit, it, key=key)  # type: ignore
     return sorted(it, key=key, reverse=True)  # type: ignore
@@ -188,10 +191,13 @@ def extract_one(
     score_cutoff: int = 0,
 ) -> Optional[tuple[str, int]] | Optional[tuple[str, int, T]]:
     it = _extraction_generator(query, choices, scorer, score_cutoff)
-    key = lambda t: t[1]
+
+    def key(t):
+        return t[1]
+
     try:
         return max(it, key=key)
-    except:
+    except Exception:
         # iterator could return nothing
         return None
 
