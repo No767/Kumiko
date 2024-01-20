135fcc542a26cff1c6695a6a96f0441f7d649095
No767 <73260931+No767@users.noreply.github.com>
2024-01-19 19:09:33 -0800

    Implement new config system

--- Bot/Libs/utils/__init__.py
+++ Bot/Libs/utils/__init__.py
@@ -4,6 +4,7 @@ from .checks import (
     is_manager as is_manager,
     is_mod as is_mod,
 )
+from .config import KumikoConfig as KumikoConfig
 from .connection_checks import (
     ensure_postgres_conn as ensure_postgres_conn,
     ensure_redis_conn as ensure_redis_conn,
