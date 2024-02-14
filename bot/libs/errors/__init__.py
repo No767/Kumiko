91ebf2458da61207efa45015e451b4ba459d3501
No767 <73260931+No767@users.noreply.github.com>
2024-02-14 02:46:06 -0800

    Remove exceptions package

--- bot/libs/errors/__init__.py
+++
@@ -1,7 +0,0 @@
-from .exceptions import (
-    EconomyDisabledError as EconomyDisabledError,
-    KumikoExceptionError as KumikoExceptionError,
-    PinsDisabledError as PinsDisabledError,
-    RedirectsDisabledError as RedirectsDisabledError,
-    ValidationError as ValidationError,
-)
