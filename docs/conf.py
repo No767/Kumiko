e6f512cfc835445017c3503a4a0d5b87bdc4cd07
No767 <73260931+No767@users.noreply.github.com>
2024-11-24 21:53:03 -0800

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ html_title = "Kumiko"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []
 
 html_theme_options = {
     "dark_css_variables": {
