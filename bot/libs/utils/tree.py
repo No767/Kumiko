2b9dba53afc3a35a05d9b17eeb2fd674decba6e3
No767 <73260931+No767@users.noreply.github.com>
2024-11-25 16:38:24 -0800

    add blacklist on tree

--- bot/libs/utils/tree.py
+++ bot/libs/utils/tree.py
@@ -18,6 +18,20 @@ class KumikoCommandTree(CommandTree):
         ):
             return True
 
-        # TODO - Add blacklist feature here.
+        if interaction.user.id in bot.blacklist:
+            msg = (
+                f"My fellow user, {interaction.user.mention}, you just got the L. "
+                "You are blacklisted from using this bot. Take an \U0001f1f1, \U0001f1f1oser. "
+                "[Here is your appeal form](https://media.tenor.com/K9R9beOgPR4AAAAC/fortnite-thanos.gif)"
+            )
+            await interaction.response.send_message(msg, ephemeral=True)
+            return False
+
+        if interaction.guild and interaction.guild.id in bot.blacklist:
+            await interaction.response.send_message(
+                "This is so sad lolllllll! Your whole entire server got blacklisted!",
+                ephemeral=True,
+            )
+            return False
 
         return True
