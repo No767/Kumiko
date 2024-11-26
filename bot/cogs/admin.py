27a140f87aa5582daa766013221a94458d5b0de6
No767 <73260931+No767@users.noreply.github.com>
2024-11-25 16:32:59 -0800

    Add blacklist

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -1,18 +1,44 @@
 from __future__ import annotations
 
-from typing import (
-    TYPE_CHECKING,
-    Literal,
-    Optional,
-)
+from typing import TYPE_CHECKING, Any, AsyncIterator, Literal, Optional, TypeVar, Union
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, menus
+from libs.utils.pages import KumikoPages
 
 if TYPE_CHECKING:
     from bot.kumiko import Kumiko
 
+_T = TypeVar("_T")
+
 NO_CONTROL_MSG = "This view cannot be controlled by you, sorry!"
+
+
+class BlacklistPageSource(menus.AsyncIteratorPageSource):
+    def __init__(self, entries: dict[str, Union[_T, Any]]):
+        super().__init__(self.blacklist_iterator(entries), per_page=20)
+
+    async def blacklist_iterator(
+        self, entries: dict[str, Union[_T, Any]]
+    ) -> AsyncIterator[str]:
+        for key, entry in entries.items():
+            yield f"{key}: {entry}"
+
+    async def format_page(
+        self, menu: "BlacklistPages", entries: list[str]
+    ) -> discord.Embed:
+        pages = []
+        for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
+            pages.append(f"{index + 1}. {entry}")
+
+        menu.embed.description = "\n".join(pages)
+        return menu.embed
+
+
+class BlacklistPages(KumikoPages):
+    def __init__(self, entries: dict[str, Union[_T, Any]], *, ctx: commands.Context):
+        super().__init__(BlacklistPageSource(entries), ctx=ctx)
+        self.embed = discord.Embed(colour=discord.Colour.from_rgb(200, 168, 255))
 
 
 class Admin(commands.Cog, command_attrs=dict(hidden=True)):
@@ -62,6 +88,32 @@ class Admin(commands.Cog, command_attrs=dict(hidden=True)):
                 ret += 1
 
         await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+    @commands.guild_only()
+    @commands.group(name="blacklist", invoke_without_command=True)
+    async def blacklist(self, ctx: commands.Context) -> None:
+        """Global blacklisting system - Without subcommand you are viewing the blacklist"""
+        entries = self.bot.blacklist.all()
+        if len(entries) == 0:
+            await ctx.send("No blacklist entries found")
+            return
+
+        pages = BlacklistPages(entries, ctx=ctx)
+        await pages.start()
+
+    @blacklist.command(name="add")
+    async def add(self, ctx: commands.Context, id: discord.Object) -> None:
+        """Adds an ID to the global blacklist"""
+        given_id = id.id
+        await self.bot.add_to_blacklist(given_id)
+        await ctx.send(f"Done. Added ID {given_id} to the blacklist")
+
+    @blacklist.command(name="remove")
+    async def remove(self, ctx: commands.Context, id: discord.Object) -> None:
+        """Removes an ID from the global blacklist"""
+        given_id = id.id
+        await self.bot.remove_from_blacklist(given_id)
+        await ctx.send(f"Done. Removed ID {given_id} from the blacklist")
 
 
 async def setup(bot: Kumiko) -> None:
--- bot/libs/utils/config.py
+++ bot/libs/utils/config.py
@@ -1,6 +1,10 @@
+import asyncio
+import os
+import uuid
 from pathlib import Path
 from typing import Any, Dict, Generic, Optional, TypeVar, Union, overload
 
+import msgspec
 import yaml
 
 _T = TypeVar("_T")
@@ -44,3 +48,81 @@ class KumikoConfig(Generic[_T]):
 
     def all(self) -> dict[str, Union[_T, Any]]:
         return self._config
+
+
+class Blacklist(Generic[_T]):
+    """Internal blacklist database used by R. Danny"""
+
+    def __init__(
+        self,
+        path: Path,
+        *,
+        load_later: bool = False,
+    ):
+        self.path = path
+        self.encoder = msgspec.json.Encoder()
+        self.loop = asyncio.get_running_loop()
+        self.lock = asyncio.Lock()
+        self._db: dict[str, Union[_T, Any]] = {}
+        if load_later:
+            self.loop.create_task(self.load())
+        else:
+            self.load_from_file()
+
+    def load_from_file(self):
+        try:
+            with open(self.path, "r", encoding="utf-8") as f:
+                self._db = msgspec.json.decode(f.read())
+        except FileNotFoundError:
+            self._db = {}
+
+    async def load(self):
+        async with self.lock:
+            await self.loop.run_in_executor(None, self.load_from_file)
+
+    def _dump(self):
+        temp = f"{uuid.uuid4()}-{self.path.stem}.tmp"
+        with open(temp, "w", encoding="utf-8") as tmp:
+            encoded = msgspec.json.format(
+                self.encoder.encode(self._db.copy()), indent=2
+            )
+            tmp.write(encoded.decode())
+
+        # atomically move the file
+        os.replace(temp, self.path)
+
+    async def save(self) -> None:
+        async with self.lock:
+            await self.loop.run_in_executor(None, self._dump)
+
+    @overload
+    def get(self, key: Any) -> Optional[Union[_T, Any]]: ...
+
+    @overload
+    def get(self, key: Any, default: Any) -> Union[_T, Any]: ...
+
+    def get(self, key: Any, default: Any = None) -> Optional[Union[_T, Any]]:
+        """Retrieves a config entry."""
+        return self._db.get(str(key), default)
+
+    async def put(self, key: Any, value: Union[_T, Any]) -> None:
+        """Edits a config entry."""
+        self._db[str(key)] = value
+        await self.save()
+
+    async def remove(self, key: Any) -> None:
+        """Removes a config entry."""
+        del self._db[str(key)]
+        await self.save()
+
+    def __contains__(self, item: Any) -> bool:
+        return str(item) in self._db
+
+    def __getitem__(self, item: Any) -> Union[_T, Any]:
+        return self._db[str(item)]
+
+    def __len__(self) -> int:
+        return len(self._db)
+
+    def all(self) -> dict[str, Union[_T, Any]]:
+        return self._db
